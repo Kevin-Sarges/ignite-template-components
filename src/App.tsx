5bf514147f9ef6fd5c61f5a5aca53fe2d8728a41
Kevin-Sarges <kevinsarges08@gmail.com>
2022-02-09 20:01:23 -0300

    Finalizando componente de SideBar

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { Button } from "./components/Button";
 import { MovieCard } from "./components/MovieCard";
 
-// import { SideBar } from './components/SideBar';
+import { SideBar } from "./components/SideBar";
 import { Content } from "./components/Content";
 
 import { api } from "./services/api";
@@ -66,23 +66,17 @@ export function App() {
   return (
     <div style={{ display: "flex", flexDirection: "row" }}>
       {/*Side bar */}
-      <nav className="sidebar">
-        <span>
-          Watch<p>Me</p>
-        </span>
-
-        <div className="buttons-container">
-          {genres.map((genre) => (
-            <Button
-              key={String(genre.id)}
-              title={genre.title}
-              iconName={genre.name}
-              onClick={() => handleClickButton(genre.id)}
-              selected={selectedGenreId === genre.id}
-            />
-          ))}
-        </div>
-      </nav>
+      <SideBar
+        contentContainer={genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      />
 
       {/*Container */}
       <Content
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,17 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+interface SideBarProps {
+  contentContainer: Array<{}>;
+}
+
+export function SideBar(props: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">{props.contentContainer}</div>
+    </nav>
+  );
 }
